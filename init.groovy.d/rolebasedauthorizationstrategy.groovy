c3b65abafcbe580a66b9d82e97755825ec1658cf
Adrian Bienkowski <adrian.bienkowski@ethoca.com>
2017-09-26 19:50:29 +0000

    Add script to setup role based authentication on initialization.

---
+++ init.groovy.d/rolebasedauthorizationstrategy.groovy
@@ -0,0 +1,60 @@
+import com.michelin.cio.hudson.plugins.rolestrategy.Role;
+import com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy;
+import hudson.security.Permission;
+import jenkins.model.Jenkins;
+
+def env = System.getenv()
+def sid = env.ADMIN_USERNAME
+
+/* get Jenkins instance reference */
+def instance = Jenkins.getInstance()
+
+/* create RoleBasedAuthorizationStrategy instance */
+def roleBasedAuthorization = new RoleBasedAuthorizationStrategy();
+
+/* create admin set of permissions */
+Set<Permission> adminPermissions = new HashSet<Permission>();
+adminPermissions.add(Permission.fromId("hudson.model.View.Delete"));
+adminPermissions.add(Permission.fromId("hudson.model.Computer.Connect"));
+adminPermissions.add(Permission.fromId("hudson.model.Run.Delete"));
+adminPermissions.add(Permission.fromId("hudson.model.Hudson.UploadPlugins"));
+adminPermissions.add(Permission.fromId("hudson.model.Computer.Create"));
+adminPermissions.add(Permission.fromId("hudson.model.View.Configure"));
+adminPermissions.add(Permission.fromId("hudson.model.Hudson.ConfigureUpdateCenter"));
+adminPermissions.add(Permission.fromId("hudson.model.Computer.Build"));
+adminPermissions.add(Permission.fromId("hudson.model.Item.Configure"));
+adminPermissions.add(Permission.fromId("hudson.model.Hudson.Administer"));
+adminPermissions.add(Permission.fromId("hudson.model.Item.Cancel"));
+adminPermissions.add(Permission.fromId("hudson.model.Item.Read"));
+adminPermissions.add(Permission.fromId("hudson.model.Computer.Delete"));
+adminPermissions.add(Permission.fromId("hudson.model.Item.Build"));
+adminPermissions.add(Permission.fromId("hudson.scm.SCM.Tag"));
+adminPermissions.add(Permission.fromId("hudson.model.Item.Discover"));
+adminPermissions.add(Permission.fromId("hudson.model.Hudson.Read"));
+adminPermissions.add(Permission.fromId("hudson.model.Item.Create"));
+adminPermissions.add(Permission.fromId("hudson.model.Item.Move"));
+adminPermissions.add(Permission.fromId("hudson.model.Item.Workspace"));
+adminPermissions.add(Permission.fromId("hudson.model.View.Read"));
+adminPermissions.add(Permission.fromId("hudson.model.Hudson.RunScripts"));
+adminPermissions.add(Permission.fromId("hudson.model.View.Create"));
+adminPermissions.add(Permission.fromId("hudson.model.Item.Delete"));
+adminPermissions.add(Permission.fromId("hudson.model.Computer.Configure"));
+adminPermissions.add(Permission.fromId("hudson.model.Computer.Disconnect"));
+adminPermissions.add(Permission.fromId("hudson.model.Run.Update"));
+
+/* create the admin role */
+Role adminRole = new Role('admin', adminPermissions);
+roleBasedAuthorization.addRole(RoleBasedAuthorizationStrategy.GLOBAL, adminRole);
+
+/* assign admin role to the selected user */
+roleBasedAuthorization.doAssignRole(
+	RoleBasedAuthorizationStrategy.GLOBAL,
+	'admin',
+	sid
+);
+
+/* set Authorization strategy */
+instance.setAuthorizationStrategy(roleBasedAuthorization)
+
+/* save the configuration */
+instance.save()
