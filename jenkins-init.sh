a918c8bbf4b5a8c47da1f345bd451def2ae3d989
Adrian Bienkowski <adrian.bienkowski@ethoca.com>
2017-09-28 22:09:50 +0000

    Add automatic creating of known_hosts file for gitlab host.

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -19,10 +19,13 @@ done
 export LDAP_MANAGER_DN="$(cat /run/secrets/LDAP_MANAGER_DN)"
 export LDAP_MANAGER_KEY="$(cat /run/secrets/LDAP_MANAGER_KEY)"
 
-# -- create private ssh key from secrets
+# -- create private ssh key from secrets and create known_hosts file
 mkdir -p $JENKINS_HOME/.ssh
 cat /run/secrets/GIT_SSH_KEY > $JENKINS_HOME/.ssh/id_rsa
 chmod 600 $JENKINS_HOME/.ssh/id_rsa
+if [[ -z $GIT_HOSTNAME ]]; then
+    ssh-keyscan $GIT_HOSTNAME >> ~/.ssh/known_hosts
+fi
 
 # -- install all required plugins
 /usr/local/bin/install-plugins.sh < /usr/share/jenkins/sidekick/plugins.txt
